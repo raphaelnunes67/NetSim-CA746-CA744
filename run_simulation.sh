c2d5c712ec75ebba2831a5785f0473886d29d6ae
Raphael Nunes <raphaelnunes67@gmail.com>
2024-11-21 23:45:52 -0400

    feat: update bash

--- run_simulation.sh
+++ run_simulation.sh
@@ -7,6 +7,6 @@ COMMAND="PYTHONPATH=\"$PWD/..\" python3 main.py -c ca746 -ev 0 -pv 20 -l 1"
 # Run the command 1000 times
 for i in {1..1000}
 do
-    echo "Running simulation $i"
+    echo "-----------Running simulation $i-----------------"
     eval $COMMAND
 done
