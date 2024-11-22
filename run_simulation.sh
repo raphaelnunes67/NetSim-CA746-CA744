26558a6a5a67b64aaf058d3e27866c69210908f6
Raphael Nunes <raphaelnunes67@gmail.com>
2024-11-21 23:49:41 -0400

    hotfix: add venv

--- run_simulation.sh
+++ run_simulation.sh
@@ -2,7 +2,7 @@
 
 cd src
 
-COMMAND="PYTHONPATH=\"$PWD/..\" python3 main.py -c ca746 -ev 0 -pv 20 -l 1"
+COMMAND="PYTHONPATH=\"$PWD/..\" ../venv/bin/python3 main.py -c ca746 -ev 0 -pv 20 -l 1"
 
 # Run the command 1000 times
 for i in {1..1000}
